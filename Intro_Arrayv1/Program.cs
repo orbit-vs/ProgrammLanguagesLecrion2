0fd520a5e3f4e1d1735753428b6b4a043d4de937
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 15:11:55 +0300

    Intro_Arrayv1

---
+++ Intro_Arrayv1/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1,12,31,4,15,16,17,8 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index <n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index +1;
+    index++;
+}
